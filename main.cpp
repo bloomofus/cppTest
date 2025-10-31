6050001f1a8a7a3da574b12493a6bbbf290f20e4
mzz <3108482097@qq.com>
2025-10-31 15:54:10 +0800

    2025年10月31日 刷题刷到二叉树：中序遍历和后序遍历建树

--- main.cpp
+++ main.cpp
@@ -3,12 +3,54 @@
 #include "mytree.h"
 
 using namespace std;
-
 class Solution
 {
 public:
-    vector<string> binaryTreePaths(TreeNode *root)
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder)
     {
+        deque<int> indeq = vec2deq(inorder);
+        deque<int> postdeq = vec2deq(postorder);
+        return createTree(indeq, postdeq);
+    }
+
+    deque<int> vec2deq(vector<int>& vec){
+        deque<int> deq{};
+        for(auto node:vec)
+            deq.push_back(node);
+        return deq;
+    }
+
+    TreeNode* createTree(deque<int>& indeq,deque<int>& postdeq){
+        if(indeq.empty())
+            return nullptr;
+        else if (indeq.size() == 1)
+        {
+            //说明建树快结束了
+            TreeNode *lnode = new TreeNode(indeq.front(),nullptr,nullptr);
+            return lnode;
+        }
+        else{
+            int mnode_idx = 0;
+            while (mnode_idx<indeq.size()&&indeq[mnode_idx]==postdeq[mnode_idx])
+                ++mnode_idx;
+            mnode_idx = mnode_idx == indeq.size() ? mnode_idx - 1 : mnode_idx;
+            TreeNode *mnode = new TreeNode(indeq[mnode_idx], nullptr, nullptr);
+            TreeNode *cur = mnode;
+            for (int i = mnode_idx - 1; i >= 0; --i)
+            {
+                cur->left = new TreeNode(indeq[i], nullptr, nullptr);
+                cur = cur->left;
+            }
+            if (mnode_idx==indeq.size())
+                return mnode;
+            for (int i = 0; i < mnode_idx;++i){
+                indeq.pop_front();
+                postdeq.pop_front();
+            }
+            indeq.pop_front();
+            mnode->right = createTree(indeq, postdeq);
+            return mnode;
+        }
     }
 };
 
@@ -25,5 +67,12 @@ int main()
     TreeNode *rootl = new TreeNode(2,nullptr,nullptr);
     TreeNode *rootr = new TreeNode(3, nullptr, nullptr);
     TreeNode *root = new TreeNode(1,rootl,rootr);
-    cout << so.binaryTreePaths(t.root);
+
+    vector<int> in{1,2};
+    vector<int> post{2,1};
+    tree myt;
+    myt.root = so.buildTree(in, post);
+    myt.midshow(myt.root);
+    cout << endl;
+    myt.postshow(myt.root);
 }
